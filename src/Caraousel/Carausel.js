113f334e7e4873d68e82261e9f63d7d2779a042a
Amartya <amarkumargta@gmail.com>
2021-09-28 17:55:13 +0530

    used react-responsive-carausel

--- src/Caraousel/Carausel.js
+++ src/Caraousel/Carausel.js
@@ -1,41 +1,37 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import model from "../assets/model.jpg"
+import model2 from "../assets/model2.jpeg"
+import model3 from "../assets/model3.jpg"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Carousel} from "react-bootstrap"
-import dol from "../assets/dol.jpg"
-import r from "../assets/r.jpg"
-import "./car.css"
-import q from "../assets/q.jpeg"
 
 
-const Caraus = () => {
-    return (
-      <Carousel >
-      <Carousel.Item  >
-        <img
-          className="img d-block w-100"
-          src={dol}
-          alt="First slide"
-        />
-        
-      </Carousel.Item>
-      <Carousel.Item >
-        <img
-          className="d-block w-100"
-          src={r}
-          alt="Second slide"
-        />
+
+export default function SimpleSlider() {
+  
+  return (
+    <div className="container-fluid">
+      <div className="row">
        
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={q}
-          alt="Third slide"
-        />
-        
-      </Carousel.Item>
+    <div class="carousel-wrapper ms-5 mt-5" style={{width:"50em",position:"relative",left:"15em",top:"3em"}}>
+    <Carousel infiniteLoop useKeyboardArrows autoPlay>
+      <div>
+        <img src={model} />
+        <p className="legend">Legend 1</p>
+    </div>
+    <div>
+        <img src={model2} />
+    </div>
+    <div>
+        <img src={model3} />
+        <p className="legend">Legend 3</p>
+    </div>
     </Carousel>
-    )
-}
+    </div>
+    </div>
+    </div>
+  
 
-export default Caraus
+  )}
